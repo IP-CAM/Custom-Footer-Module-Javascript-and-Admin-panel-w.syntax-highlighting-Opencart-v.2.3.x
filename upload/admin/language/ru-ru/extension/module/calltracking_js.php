8bebf7c570d522d166d80d5ee95c9ba986218d8f
Ihor Chyshkala <ihor@chyshkala.com>
2018-11-28 22:00:26 +0200

    Initial commit

---
+++ upload/admin/language/ru-ru/extension/module/calltracking_js.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * Russian 
+ * 
+ */
+
+ // Heading
+$_['heading_title']    = 'Calltrackingjs';
+
+// Text
+$_['text_module']      = 'Модули';
+$_['text_total']       = 'Всего заказов';
+$_['text_payment']     = 'Оплата';
+$_['text_shipping']    = 'Доставка';
+$_['text_feed']        = 'Канал продвижения';
+$_['text_success']     = 'Настройки модуля Calltrackingjs обновлены!';
+$_['placeholder']      = 'Поздравляем! Вы успешно ';
+
+// Error
+$_['error_permission'] = 'Ошибка: У Вас нет прав на изменение настроек модуля Calltrackingjs!';
+
+?>
