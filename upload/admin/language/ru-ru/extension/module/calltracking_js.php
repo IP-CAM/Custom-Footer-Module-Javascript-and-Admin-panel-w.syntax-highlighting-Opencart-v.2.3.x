2b117b165c6a509d0672f32d7265ec5f26ebffc2
Ihor Chyshkala <ihor@chyshkala.com>
2018-12-07 15:12:03 +0200

    module ready

--- upload/admin/language/ru-ru/extension/module/calltracking_js.php
+++ upload/admin/language/ru-ru/extension/module/calltracking_js.php
@@ -14,8 +14,14 @@ $_['text_total']       = 'Всего заказов';
 $_['text_payment']     = 'Оплата';
 $_['text_shipping']    = 'Доставка';
 $_['text_feed']        = 'Канал продвижения';
+$_['text_disabled']    = 'Выключено';
+$_['text_enabled']     = 'Включено';
+$_['entry_code']       = 'Код CallTracking';
+$_['error_code']       = 'Вы не ввели код для CallTracking!';
+$_['entry_status']     = 'Статус модуля';
 $_['text_success']     = 'Настройки модуля Calltrackingjs обновлены!';
-$_['placeholder']      = 'Поздравляем! Вы успешно ';
+$_['placeholder']      = 'Поздравляем! Вы успешно установили модуль';
+$_['text_signup']      = 'Введите текст CallTracking, который вы получили от провайдера или написали сами';
 
 // Error
 $_['error_permission'] = 'Ошибка: У Вас нет прав на изменение настроек модуля Calltrackingjs!';
--- upload/catalog/controller/extension/module/calltracking_js.php
+++ upload/catalog/controller/extension/module/calltracking_js.php
@@ -4,24 +4,9 @@ class ControllerExtensionModuleCalltrackingJS extends Controller {
 
 	protected function index() {
 
-		//Load language file
-		$this->language->load('extension/module/calltracking_js');
+		$code = html_entity_decode($this->config->get('calltrackingjs_code'), ENT_QUOTES, 'UTF-8');
 
-		//Set title from language file
-      	$data['heading_title'] = $this->language->get('heading_title');
-
-		//Load model
-		$this->load->model('extension/module/calltracking_js');
-
-		//Sample - get data from loaded model
-		$data['customers'] = $this->model_extension_module_calltracking_js->getCustomerData();
-
-		//Select template
-		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/extension/module/calltracking_js.tpl')) {
-			$this->response->setOutput($this->load->view('extension/module/calltracking_js.tpl', $data));
-		} else {
-			$this->response->setOutput($this->load->view('extension/..module/calltracking_js.tpl', $data));
-		}
+		$this->document->setCallTracking($code);
 
 	}
 }
